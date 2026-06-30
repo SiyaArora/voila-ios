4e2d858ba1c10e7c1ce9af36128ef9665c020c49
Siya <sarora18@dons.usfca.edu>
2026-06-30 14:01:11 -0700

    Initial Commit

---
+++ voila/voilaApp.swift
@@ -0,0 +1,17 @@
+//
+//  voilaApp.swift
+//  voila
+//
+//  Created by Siya Arora on 6/30/26.
+//
+
+import SwiftUI
+
+@main
+struct voilaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
